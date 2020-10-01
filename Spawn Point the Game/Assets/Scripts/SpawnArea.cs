862078106d2dd004b1a2101746743d0da0444c4b
Thomas Stassin <thomas@panopticgame.com>
2020-10-01 11:21:16 +0200

    add project to the repo

---
+++ Spawn Point the Game/Assets/Scripts/SpawnArea.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnArea : MonoBehaviour
+{
+
+    public GameObject monster;
+
+    [Range(0f, 5f)]
+    public float radius=1f;
+
+    public GameObject Spawn() {
+        if(monster == null) {
+            Debug.LogError("No monster set in " + gameObject.name, gameObject);
+            return null;
+        }
+        Vector3 position = transform.position;
+
+        Vector3 rnd = Random.insideUnitSphere * radius;
+        rnd.y = 0;
+        position += rnd;
+
+        GameObject newMonster = Instantiate(monster, position, Quaternion.identity);
+
+        return newMonster;
+    }
+
+    private void OnDrawGizmosSelected() {
+        Gizmos.color = new Color(0f, 0f, 1f, 0.25f);
+        Gizmos.DrawWireSphere(transform.position, radius);
+    }
+
+
+}
